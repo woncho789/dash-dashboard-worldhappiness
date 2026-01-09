edc3a67087bb8c22209d36f0a685881a489d6df9
woncho789 <100104817+woncho789@users.noreply.github.com>
2026-01-08 21:33:40 -0500

    Update 5. Chaining callbacks inputs and outputs.py

--- 5. Chaining callbacks inputs and outputs.py
+++ 5. Chaining callbacks inputs and outputs.py
@@ -49,7 +49,7 @@ def update_country_dropdown(selected_region):
 def update_graph(selected_country, selected_data):
     filtered_happiness = happiness[happiness['country']== selected_country]
     line_fig = px.line(filtered_happiness,
-                       x='year', y='happiness_score',
+                       x='year', y=selected_data,
                        title = f'{selected_data.title()} in {selected_country}')
     selected_avg = filtered_happiness[selected_data].mean()
     return line_fig, f'The average {selected_data} for {selected_country}' \
